f841565d579cb1b7bc40cf602202df6e169df4da
LeoneCarmelo <carmeloleone@outlook.it>
2023-04-17 16:18:12 +0200

    Add call Api + header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup>
 
+<script>
+import axios from 'axios'
+export default {
+   data() {
+      return {
+         urlApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=5&offset=0',
+         cards: null
+      }
+   },
+   methods: {
+
+   },
+   mounted() {
+      axios
+         .get(this.urlApi)
+         .then(response => {
+            console.log(response.data.data)
+            this.cards = response.data.data
+         })
+   }
+}
 </script>
 
 <template>
-<h1>Hello world</h1>
+   <header class="d-flex p-2">
+      <div class="logo m-2">
+         <img src="/img/logo-yu-gi-oh.png" alt="" class="img-fluid">
+      </div>
+      <h1 class="ms-3">Yu-Gi-Oh Api</h1>
+
+   </header>
+   <main>
+   </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.logo {
+   width: 180px;
 
+}
 </style>
