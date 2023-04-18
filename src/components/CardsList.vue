437c24c35a3e281c4ec3c488a7f510895e1e87f7
LeoneCarmelo <carmeloleone@outlook.it>
2023-04-18 17:18:06 +0200

    Add style

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <template>
-    <ul class="card_list row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 ps-0  justify-content-between">
+    <ul class="card_list  row row-cols-1 row-cols-sm-2 row-cols-md-5 ps-0 justify-content-between ">
         <SingleCard v-for="card in store.cards" :src="card.card_images[0].image_url" :title="card.name"
             :archetype="card.archetype" />
     </ul>
--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -12,19 +12,27 @@ export default {
 </script>
 
 <template>
-    <li class="col px-0">
+    <li class="col px-0 mb-3 rounded-2">
         <div class="card">
-            <img :src="src" alt="">
+            <img :src="src" alt="" class="rounded-2">
         </div>
-        <div class="card_title text-center text-uppercase fw-bold text-white">{{ title }}</div>
-        <div class="card_archetype text-center">{{ archetype }}</div>
+        <div class="card_body">
+            <div class="card_title text-center text-uppercase fw-bold text-white">{{ title }}</div>
+            <div class="card_archetype text-center">{{ archetype }}</div>
+        </div>
     </li>
 </template>
 
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+
 .col {
     background-color: $primary;
+    width: 200px
+}
+
+.col:hover {
+    box-shadow: 0 0 2px 2px $primary;
 }
 </style>
